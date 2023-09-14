28e83431e690efa577a5c22e0af2d1d56596886a
git <mapi@sangut.cl>
2023-09-13 23:09:57 -0300

    feat(multimedia): add chosen image and video

---
+++ src/multimedia/HomeMultimedia.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+function HomeMultimedia() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const handleUploadClick = () => {
+    if (!file) {
+      return;
+    }
+
+    // 👇 Uploading the file using the fetch API to the server
+    console.log(file)
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+
+      <div>{file && `${file.name} - ${file.type}`}</div>
+
+      <button onClick={handleUploadClick}>Upload</button>
+    </div>
+  );
+}
+
+export default HomeMultimedia;
---
+++ src/multimedia/UploadVideo.jsx
@@ -0,0 +1,43 @@
+import {useState, useEffect} from "react";
+
+const UploadVideo = () => {
+    const [selectedFile, setSelectedFile] = useState()
+    const [preview, setPreview] = useState()
+
+    // create a preview as a side effect, whenever selected file is changed
+    useEffect(() => {
+        if (!selectedFile) {
+            setPreview(undefined)
+            return
+        }
+
+        const objectUrl = URL.createObjectURL(selectedFile)
+        setPreview(objectUrl)
+
+        // free memory when ever this component is unmounted
+        return () => URL.revokeObjectURL(objectUrl)
+    }, [selectedFile, setSelectedFile])
+
+    const onSelectFile = e => {
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectedFile(undefined)
+            return
+        }
+
+        // I've kept this example simple by using the first image instead of multiple
+        setSelectedFile(e.target.files[0])
+    }
+
+    return (
+        <div>
+            <input type='file' accept="video/*" onChange={onSelectFile} />
+            {selectedFile && preview &&
+                <video width="400" controls>
+                    <source src={preview}/>
+                </video>
+            }
+        </div>
+    )
+}
+
+export default UploadVideo;
